7b3ae740bdc4fc711b950f00618d1db0e360b11b
TricksyPixel <KierEmJ.II@gmail.com>
2025-04-18 13:21:06 +0800

    Cleaning up resources folder from unused graphics.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -83,39 +83,9 @@ namespace astronomical_processing_application.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap cross_small {
-            get {
-                object obj = ResourceManager.GetObject("cross-small", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap minimize {
             get {
                 object obj = ResourceManager.GetObject("minimize", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap space_4984262_1280 {
-            get {
-                object obj = ResourceManager.GetObject("space-4984262_1280", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap transparent {
-            get {
-                object obj = ResourceManager.GetObject("transparent", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
